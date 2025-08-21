481f611bd511be2e2e64d358918b2cb5a3e400ce
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-21 18:35:36 +0000

    Add files via upload

---
+++ 004_area.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int length,width,area_of_rect;
+  length = 10;
+  width = 20;
+  area_of_rect= length*width;
+  printf("area is%d\n", area_of_rect);
+  return 0;
+}
