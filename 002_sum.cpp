d1db6fab77b94159645a8d867b9b80c51e286b73
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-21 18:09:42 +0000

    Add files via upload

---
+++ 002_sum.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("enter num 1 :");
+  scanf("%d",&num1);
+  printf("enter num 2 :");
+  scanf("%d",&num2);
+  sum=num1+num2;
+  printf("sum is:");
+  return 0;
+}
