0e5bcd5045c2d0ccb58b83feafda6a0e691278d8
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-22 14:09:15 +0000

    Add files via upload

---
+++ 007_positive_negetive_zero.cpp.cc
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int num ;
+  printf("enter a number: \n");
+  scanf("%d",&num);
+  if(num>0)
+  {
+    printf("number is positive");
+  }
+  else if(num<0)
+  {
+    printf("number is negetive");
+  }
+  else
+  {
+    printf(" number is zero");
+  }
+  return 0;
+}
