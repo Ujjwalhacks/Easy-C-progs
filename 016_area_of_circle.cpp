af7b0fb98a9830a22d3ab8c661d6ce9f5f853911
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-25 15:45:38 +0000

    Add files via upload

---
+++ 016_area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int rad,area;
+  printf("enter the radius of the circle:");
+  scanf("%d",&rad);
+  area=3.14*rad*rad;
+  printf("Area is :%d",area);
+  return 0;
+}
