a196c60a6c3986720ec10b198ac1973fa47bb9ac
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-25 15:52:19 +0000

    Add files via upload

---
+++ 017_cir_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  float rad,cir;
+  printf("enter the radius of the circle:");
+  scanf("%f",&rad);
+  cir=2*(3.14*rad);
+  printf("Circumference  is :%f",cir);
+  return 0;
+}
