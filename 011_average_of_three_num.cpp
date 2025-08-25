dbc4e0af721cd2a0966c6c86f5fec48f9c5c797b
UJJWAL KRISHNA U <86040601+Ujjwalhacks@users.noreply.github.com>
2025-08-25 15:12:05 +0000

    Add files via upload

---
+++ 011_average_of_three_num.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,num3,avg;
+  printf("enter num1:");
+  scanf("%d",&num1);
+  printf("enter num2:");
+  scanf("%d",&num2);
+  printf("enter num3:");
+  scanf("%d",&num3);
+  avg=(num1+num2+num3)/3;
+  printf("average is :%d",avg);
+  return 0;
+}
